b0ba2e8c8d9cac04870c8adc906703565b938235
abhi <abhionly009@gmail.com>
2020-04-13 20:21:51 +0530

    added code to create repository, service and controller method for User

--- src/main/java/com/av4/user/Controller/UserDetailsController.java
+++ src/main/java/com/av4/user/Controller/UserDetailsController.java
@@ -1,8 +1,12 @@
-package com.av4.user.Controller;
+package com.av4.user.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.av4.user.model.UserInfo;
 
 @RestController
 @RequestMapping("/user")
@@ -14,4 +18,20 @@ public class UserDetailsController {
 		return "Welcome here";
 	}
 
+	
+	@PostMapping("/register")
+	public UserInfo registerUser(@RequestBody UserInfo userInfo) {
+		
+		System.out.println(userInfo.getName());
+		System.out.println(userInfo.getAddress());
+		System.out.println(userInfo.getEmail());
+		System.out.println(userInfo.getCompany());
+		System.out.println(userInfo.getMobile());
+		
+		System.out.println(userInfo.isEmailVerified());
+		System.out.println(userInfo.isMobileVerified());
+
+
+		return userInfo; 
+	}
 }
