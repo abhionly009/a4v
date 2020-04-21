ef878a9a4aea44f2985fb20c6ff8fcbe01dafb7a
abhi <abhionly009@gmail.com>
2020-04-21 12:59:55 +0530

    added code to check

--- src/main/java/com/av4/user/Controller/UserDetailsController.java
+++ src/main/java/com/av4/user/Controller/UserDetailsController.java
@@ -5,22 +5,39 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.av4.user.model.UserInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 @RestController
 @RequestMapping("/user")
 public class UserDetailsController {
 
-	
+	Logger logger = LoggerFactory.getLogger(UserDetailsController.class);
 	@GetMapping
 	public String getInfo() {
-		return "Welcome here";
+		
+//		final String uri = "http://localhost:8080/check/id";
+//	     
+//	    RestTemplate restTemplate = new RestTemplate();
+//	    String result = restTemplate.getForObject(uri, String.class);
+//	     
+//	    System.out.println(result);
+//	    
+//	    
+	    return "resukt";
 	}
 
 	
 	@PostMapping("/register")
 	public UserInfo registerUser(@RequestBody UserInfo userInfo) {
+	logger.info(userInfo.getAddress());
+	logger.debug("DEBUG");
+	
+	logger.error("Error");
 		return userInfo; 
 	}
 }
