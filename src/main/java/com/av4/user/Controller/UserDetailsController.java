585e014b1071f969256aa211291c4ae9393b9d97
abhi <abhionly009@gmail.com>
2020-04-21 13:11:13 +0530

    Added code to register new user

--- src/main/java/com/av4/user/Controller/UserDetailsController.java
+++ src/main/java/com/av4/user/Controller/UserDetailsController.java
@@ -1,9 +1,13 @@
-package com.av4.user.Controller;
+package com.av4.user.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.av4.user.model.UserInfo;
 
 @RestController
 @RequestMapping("/user")
@@ -14,9 +18,22 @@ public class UserDetailsController {
 	public String getInfo() {
 		return "Welcome here";
 	}
+
+
 	
 	@PostMapping("/register")
-	public String registerUser() {
-		return "User has been successfully";
+	public UserInfo registerUser(@RequestBody UserInfo userInfo) {
+		
+		System.out.println(userInfo.getName());
+		System.out.println(userInfo.getAddress());
+		System.out.println(userInfo.getEmail());
+		System.out.println(userInfo.getCompany());
+		System.out.println(userInfo.getMobile());
+		
+		System.out.println(userInfo.isEmailVerified());
+		System.out.println(userInfo.isMobileVerified());
+
+
+		return userInfo; 
 	}
 }
---
+++ src/main/java/com/av4/user/model/UserInfo.java
@@ -0,0 +1,78 @@
+package com.av4.user.model;
+
+public class UserInfo {
+	
+	private String name;
+	
+	private String email;
+	
+	private String mobile;
+	
+	private String address;
+	
+	private String company;
+	
+	private boolean isMobileVerified;
+	
+	private boolean isEmailVerified;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCompany() {
+		return company;
+	}
+
+	public void setCompany(String company) {
+		this.company = company;
+	}
+
+	public boolean isMobileVerified() {
+		return isMobileVerified;
+	}
+
+	public void setMobileVerified(boolean isMobileVerified) {
+		this.isMobileVerified = isMobileVerified;
+	}
+
+	public boolean isEmailVerified() {
+		return isEmailVerified;
+	}
+
+	public void setEmailVerified(boolean isEmailVerified) {
+		this.isEmailVerified = isEmailVerified;
+	}
+	
+	
+	
+
+}
